079caff720cb0391ff05f803cc628a44a26490f7
jimaa maya <jimaa.maya@gmail.com>
2023-03-18 12:06:30 +0300

    phase-1-arthimetic-lab-done

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,6 +6,9 @@ const expect = chai.expect;
 describe('index.js', function() {
   describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
+      const num1 = 31;
+      const num2 = 2;
+      const multiply = num1 * num2;
       expect(num1).to.be.a('number')
       expect(num2).to.be.a('number')
       expect(multiply).to.eq(62)
@@ -13,12 +16,16 @@ describe('index.js', function() {
   });
   describe('random', function() {
     it("generates a random integer greater than 0", function() {
+      const random = Math.floor(Math.random() * 100) + 1;
       expect(Number.isInteger(random)).to.be.true
       expect(random).to.not.eq(0)
     });
   });
   describe('mod', function() {
     it("is an equation that calculates a remainder that is equal to 4", function() {
+      const num3 = 14;
+      const num4 = 5;
+      const mod = num3 % num4;
       expect(num3).to.be.a('number')
       expect(num4).to.be.a('number')
       expect(mod).to.eq(4)
@@ -26,6 +33,7 @@ describe('index.js', function() {
   });
   describe('max', function() {
     it("will return 20 as the highest number in the set", function() {
+      const max = Math.max(1, 5, 8, 20);
       expect(max).to.eq(20)
     });
   });
